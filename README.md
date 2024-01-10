8269b73b70e06808a4834c9de2af510607106a11
yangtory <wjdduscldrn@naver.com>
2024-01-10 13:39:19 +0900

    DBMS

---
+++ README.md
@@ -0,0 +1,3 @@
+# DBMS 프로젝트
+* MySQL 을 사용한 DBMS SQL 실습
+* MYSQL 8.0.x DBMS Server 설치 : mysql.com 에서 community version 을 다운로드 받아 설치한다.
---
+++ root(2024-01-09-Start).sql
@@ -0,0 +1,15 @@
+/* 여러줄 주석 주석문 */
+-- 한줄짜리 주석
+
+/*
+이 화면에서 SQL 명령어를 입력하고
+Ctrl + Enter 를 입력하면 명령이 실행된다
+명령어는 대체로 대소문자 구분이 없다
+주요 키워드는 대문자로, 변수 등은 소문자로 입력
+*/
+
+SHOW DATABASES;
+SHOW DATABASES;
+USE mysql; -- 나는 지금부터 mysql 를 실행할거야
+SHOW TABLES; -- mysql 스키마에 들어있는 테이블 보여죠
+SELECT * FROM user; -- user 라는 테이블로 부터 *(모든것)을 select해서 보여줘
